69198781add9b87644dd896abf05395e42693030
Poós Dániel <poosdani@gmail.com>
2022-02-19 13:26:19 +0100

    ui

---
+++ src/main/java/com/example/muzeumfrontendjavafx/Controller.java
@@ -0,0 +1,22 @@
+package com.example.muzeumfrontendjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TabPane;
+
+public class Controller {
+    @FXML private ListView<Statue> statueListView;
+    @FXML private ListView<Painting> paintingListView;
+    @FXML private TabPane tab;
+
+    public void addBtnClick() {
+    }
+
+    public void modBtnClick() {
+    }
+
+    public void delBtnClick() {
+    }
+}
