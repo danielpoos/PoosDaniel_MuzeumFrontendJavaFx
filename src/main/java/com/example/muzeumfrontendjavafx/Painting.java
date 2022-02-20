62dfd5002ca7dc97cf8704815276723f95934ee8
Poós Dániel <poosdani@gmail.com>
2022-02-20 10:11:17 +0100

    classes

--- src/main/java/com/example/muzeumfrontendjavafx/Painting.java
+++ src/main/java/com/example/muzeumfrontendjavafx/Painting.java
@@ -1,4 +1,46 @@
 package com.example.muzeumfrontendjavafx;
 
 public class Painting {
+    private Integer id;
+    private String title;
+    private int year;
+    private boolean onDisplay;
+
+    public Painting(String title, int year, boolean onDisplay) {
+        this.title = title;
+        this.year = year;
+        this.onDisplay = onDisplay;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isOnDisplay() {
+        return onDisplay;
+    }
+
+    public void setOnDisplay(boolean onDisplay) {
+        this.onDisplay = onDisplay;
+    }
 }
--- src/main/java/com/example/muzeumfrontendjavafx/Statue.java
+++ src/main/java/com/example/muzeumfrontendjavafx/Statue.java
@@ -1,4 +1,46 @@
 package com.example.muzeumfrontendjavafx;
 
 public class Statue {
+    private Integer id;
+    private String person;
+    private int height;
+    private int price;
+
+    public Statue(String person, int height, int price) {
+        this.person = person;
+        this.height = height;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
